c7b0db20889127b58fca8e6f89686323c2c4bde4
Ryan Augustyn <jraugustyn07@gmail.com>
2023-03-08 11:55:20 -0600

    complete project

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -2,9 +2,9 @@
 -- Do a full outer join to see all records from both tables matched where possible.
 -- EXPECTED RESULT: 77 Rows Returned
 
-SELECT * FROM movies LEFT JOIN users on movie_id = favorite_movie_id
-UNION
-SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
+-- SELECT * FROM movies LEFT JOIN users on movie_id = favorite_movie_id
+-- UNION
+-- SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 
 -- Because MySQL does not support "OUTER JOIN" syntax, we must use left and right joins that are brought together with "UNION".
 
@@ -13,19 +13,43 @@ SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 -- EXPECTED RESULT: 75 Rows Returned
 
 
+-- SELECT * FROM movies
+-- LEFT JOIN users 
+-- 	ON movie_id = favorite_movie_id
+
+
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a right join to see all records from the users table and matching records from the movies table.
 -- EXPECTED RESULT: 16 Rows Returned
+
+-- SELECT * FROM movies
+-- RIGHT JOIN users 
+-- 	ON movie_id = favorite_movie_id
+
+
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
 
+-- SELECT * FROM movies
+-- INNER JOIN users 
+-- 	ON movie_id = favorite_movie_id
+
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
 -- EXPECTED RESULT: Disney
+
+-- SELECT lead_studio FROM movies
+-- INNER JOIN users 
+-- 	ON movie_id = favorite_movie_id
+-- GROUP BY lead_studio
+-- ORDER BY count(lead_studio) DESC
+-- LIMIT 1
+
+    
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
@@ -33,8 +57,21 @@ SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 -- EXPECTED RESULT: 52.21
 
 
+-- SELECT avg(rotten_tomatoes) FROM movies
+-- INNER JOIN users 
+-- 	ON movie_id = favorite_movie_id
+
+
+
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
 -- EXPECTED RESULT: Mamma Mia!, Gnomeo and Juliet
 -- It is important to be able to identify dirty data within a dataset!
+
+-- SELECT film, count(film) FROM movies
+-- GROUP BY film
+-- HAVING count(film) > 1
+
+
+
 
